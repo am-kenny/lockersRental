4d27cd5781730e50dc5471f1a0c0c59034069556
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2023-11-07 01:46:51 +0100

    Add locations & lockers models to admin.py

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from locations import models
 
-# Register your models here.
+admin.site.register(models.Address)
+admin.site.register(models.Location)
+admin.site.register(models.LockerSize)
+admin.site.register(models.Locker)
+
