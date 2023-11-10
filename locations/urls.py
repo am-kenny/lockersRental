dc124e3fb13ff55f81035ab06253947789caed7a
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2023-11-10 22:48:47 +0100

    Change location urls

--- locations/urls.py
+++ locations/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='locations'),
-    path('<location_slug>', views.location, name='location'),
+    path('<location_slug>', views.location_page, name='location'),
     path('<location_slug>/available', views.available, name='available'),
-    path('<location_slug>/close', views.closed, name='close'),
-    path('<location_slug>/open', views.open, name='open'),
-    path('<location_slug>/selection', views.schedule, name='selection'),
+    path('<location_slug>/close', views.close_locker, name='close'),
+    path('<location_slug>/open', views.open_locker, name='open'),
+    path('<location_slug>/selection/<locker_size_id>', views.select_locker, name='locker_selection'),
 ]
