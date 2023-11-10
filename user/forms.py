7a2dc5bc52e52d01d929996558223e1511daef58
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2023-11-10 22:47:41 +0100

    Add BillingAddressForm

--- user/forms.py
+++ user/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from user.models import UserBillingAddress
+
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(label="Email", required=True)
@@ -14,3 +16,38 @@ class RegisterForm(UserCreationForm):
                   "last_name", "password1", "password2")
 
 
+class BillingAddressForm(forms.ModelForm):
+    country = forms.CharField(
+        label='',
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Country', 'class': 'form-control form-group'})
+    )
+    state = forms.CharField(
+        label='',
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'State', 'class': 'form-control form-group'})
+    )
+    city = forms.CharField(
+        label='',
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'City', 'class': 'form-control form-group'})
+    )
+    street = forms.CharField(
+        label='',
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Street', 'class': 'form-control form-group'})
+    )
+    house_number = forms.CharField(
+        label='',
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'House Number', 'class': 'form-control form-group'})
+    )
+    zip_code = forms.CharField(
+        label='',
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Zip Code', 'class': 'form-control form-group'})
+    )
+
+    class Meta:
+        model = UserBillingAddress
+        fields = ("country", "state", "city", "street", "house_number", "zip_code")
