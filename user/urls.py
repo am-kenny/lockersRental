e55627359f9410d7fee7f4d9235f4f2b362f7007
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2023-11-10 22:46:12 +0100

    Add billing_address to user urls

--- user/urls.py
+++ user/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('logout', views.user_logout, name='logout'),
     path('register', views.register, name='register'),
     path('rented_lockers', views.rented_lockers, name='rented_lockers'),
+    path('billing_address', views.billing_address, name='billing_address'),
+    path('billing_address/delete/<int:billing_address_id>', views.delete_billing_address, name='delete_billing_address'),
     path('billing', views.billing, name='billing'),
     ]
