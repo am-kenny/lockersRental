4a20c3fc80b62c19118649aa8c91af102b7cc552
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2023-11-10 22:45:07 +0100

    Add LOGIN_URL to settings.py

--- lockersRental/settings.py
+++ lockersRental/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+LOGIN_URL = 'login'
 STATIC_URL = 'static/'
 MEDIA_ROOT = '/media/'
 
