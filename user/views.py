2784bdf72dd4683aa098052cf5eb3fb236bf1b17
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2023-11-11 13:18:00 +0100

    Update user views, implement billing pages and forms, add billing templates

--- user/views.py
+++ user/views.py
@@ -1,13 +1,14 @@
 from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotFound
 from django.shortcuts import render, redirect
 
-from user.forms import RegisterForm
+from user.forms import RegisterForm, BillingAddressForm, BillingInfoForm
 
 
+@login_required
 def index(request):
-    if not request.user.is_authenticated:
-        return redirect('/user/login')
+
     return render(request, 'user/index.html', {})
 
 
@@ -23,6 +24,8 @@ def user_login(request):
         )
         if user is not None:
             login(request, user)
+            if request.GET.get('next'):
+                return redirect(request.GET.get('next'))
             return redirect("/")
         else:
             return HttpResponseNotFound("Failed to log in")
@@ -30,9 +33,8 @@ def user_login(request):
     return render(request, 'user/user_login.html', {})
 
 
+@login_required
 def user_logout(request):
-    if not request.user.is_authenticated:
-        return redirect('/')
     logout(request)
 
     return redirect("/user/login")
@@ -48,12 +50,86 @@ def register(request):
             return redirect('/user/login')
     else:
         form = RegisterForm()
+
     return render(request, 'user/user_register.html', {"form": form})
 
 
-def rented_lockers(request):
-    return "Rented Lockers"
+@login_required
+def billing_address(request):
+    edit_address = request.GET.get('edit_address', None)
+    edit_form = None
+
+    if request.method == 'POST':
+        form = BillingAddressForm(request.POST)
+        if form.is_valid():
+            form_data = form.save(commit=False)
+            form_data.user_id = request.user.id
+            if edit_address:
+                form_data.id = int(edit_address)
+            form_data.save()
+            return redirect('billing_address')
+    form = BillingAddressForm()
+    user_addresses = request.user.userbillingaddress_set.all()
+
+    if edit_address:
+        edit_address = int(edit_address)
+        edit_form = BillingAddressForm(instance=request.user.userbillingaddress_set.get(id=edit_address))
+
+    return render(request, 'user/billing_address.html', {
+        "user_addresses": user_addresses,
+        "form": form,
+        "edit_address": edit_address,
+        "edit_form": edit_form
+    })
 
 
+@login_required
+def delete_billing_address(request, billing_address_id):
+    query = request.user.userbillingaddress_set.filter(id=billing_address_id)
+    if query.exists():
+        query.first().delete()
+
+    return redirect('billing_address')
+
+
+@login_required
 def billing(request):
-    return "Billing"
+    edit_billing = request.GET.get('edit_billing', None)
+    edit_form = None
+
+    if request.method == 'POST':
+        form = BillingInfoForm(request.POST)
+        if form.is_valid():
+            form_data = form.save(commit=False)
+            form_data.user_id = request.user.id
+            if edit_billing:
+                form_data.id = int(edit_billing)
+            form_data.save()
+            return redirect('billing')
+    form = BillingInfoForm()
+    user_billings = request.user.userbillinginfo_set.all()
+
+    if edit_billing:
+        edit_billing = int(edit_billing)
+        edit_form = BillingInfoForm(instance=request.user.userbillinginfo_set.get(id=edit_billing))
+
+    return render(request, 'user/billing.html', {
+        "user_billings": user_billings,
+        "form": form,
+        "edit_billing": edit_billing,
+        "edit_form": edit_form
+    })
+
+
+@login_required
+def delete_billing(request, billing_id):
+    query = request.user.userbillinginfo_set.filter(id=billing_id)
+    if query.exists():
+        query.first().delete()
+
+    return redirect('billing')
+
+
+@login_required
+def rented_lockers(request):
+    return render(request, 'user/rented_lockers.html', {})
