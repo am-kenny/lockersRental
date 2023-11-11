1ca5fdeb1652c8c066e6e05dc411ac71d8ae9977
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2023-11-11 13:15:58 +0100

    Add BillingInfoForm

--- user/forms.py
+++ user/forms.py
@@ -1,8 +1,10 @@
+import datetime
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from user.models import UserBillingAddress
+from user.models import UserBillingAddress, UserBillingInfo
 
 
 class RegisterForm(UserCreationForm):
@@ -51,3 +53,39 @@ class BillingAddressForm(forms.ModelForm):
     class Meta:
         model = UserBillingAddress
         fields = ("country", "state", "city", "street", "house_number", "zip_code")
+
+
+class BillingInfoForm(forms.ModelForm):
+
+    YEAR_CHOICES = [(y, y) for y in range(datetime.date.today().year, datetime.date.today().year + 15)]
+    MONTH_CHOICE = [(m, m) for m in range(1, 13)]
+
+    card_number = forms.CharField(
+        label='',
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Card Number', 'class': 'form-control form-group'})
+    )
+
+    expiration_year = forms.ChoiceField(choices=YEAR_CHOICES,
+                                        widget=forms.Select(attrs={'class': 'form-control form-group'})
+                                        )
+
+    expiration_month = forms.ChoiceField(choices=MONTH_CHOICE,
+                                         widget=forms.Select(attrs={'class': 'form-control form-group'})
+                                         )
+
+    cvv = forms.CharField(
+        label='',
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'CVV', 'class': 'form-control form-group'})
+    )
+    billing_address = forms.ModelChoiceField(
+        label='',
+        required=True,
+        queryset=UserBillingAddress.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control form-group'})
+    )
+
+    class Meta:
+        model = UserBillingInfo
+        fields = ("card_number", "expiration_year", "expiration_month", "cvv", "billing_address")
