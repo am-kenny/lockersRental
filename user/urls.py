220f2f1f5fc45bf304415c9d2aaa3a98628d5d7c
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2023-11-14 16:19:30 +0100

    Update user urls

--- user/urls.py
+++ user/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('rented_lockers', views.rented_lockers, name='rented_lockers'),
     path('billing_address', views.billing_address, name='billing_address'),
     path('billing_address/delete/<int:billing_address_id>', views.delete_billing_address, name='delete_billing_address'),
+    path('billing/delete/<int:billing_id>', views.delete_billing, name='delete_billing'),
     path('billing', views.billing, name='billing'),
     ]
